120a17044951f6714d0d773d7e80029495fe24cb
diegoparicio <32286724+diegoparicio@users.noreply.github.com>
2024-11-02 11:04:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import calc
-
-x = calc.suma(1,2)
-print(x)
-
-y = [1,2,3,4]
-
-total_suma = calc.suma_lista(y)
-print(total_suma)
+lista1 = [0, 1, 2, 3, 4]
+lista2 = [5, 6, 7]
+lista3 = lista1[0:3]
+lista4 = lista1 + lista2
+print(f"Lista3 = {lista3}")
+print(f"Lista4 = {lista4}")
